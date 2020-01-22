0d4f9530535605e9daf17bc4fb30b4af3cd98766
Nilolai Sheryagin <nsheryagin@diasoft.ru>
2020-01-22 14:17:22 +0300

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 import App from './components/App';
 
-const store createStore(rootReducer);
+const store = createStore(rootReducer);
 
 render(
   <Provider store={store}>
